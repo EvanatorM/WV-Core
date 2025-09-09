dda026215f7987f787ab2112087b0cb0cb12a758
WSAL Evan <evan@millers6.com>
2025-09-09 11:49:20 -0400

    Quick test

---
+++ include/core/Test.h
@@ -0,0 +1,4 @@
+namespace WillowVox::Core
+{
+    void Test();
+}
---
+++ src/Test.cpp
@@ -0,0 +1,11 @@
+#include <core/Test.h>
+
+#include <iostream>
+
+namespace WillowVox::Core
+{
+    void Test()
+    {
+        std::cout << "Running Core Test..." << std::endl;
+    }
+}
