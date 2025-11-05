d7fd2941d27459088cdeac6469b03ef1583e4fe2
WSAL Evan <evan@millers6.com>
2025-11-05 12:02:03 -0500

    Updated core.h

--- include/wv/core.h
+++ include/wv/core.h
@@ -7,3 +7,16 @@
 #include <wv/app/App.h>
 
 #include <wv/assets/AssetManager.h>
+
+#include <wv/events/Event.h>
+#include <wv/events/EventDispatcher.h>
+
+#include <wv/input/Input.h>
+
+#include <wv/rendering/Camera.h>
+#include <wv/rendering/Renderer.h>
+#include <wv/rendering/Window.h>
+#include <wv/rendering/Shader.h>
+#include <wv/rendering/Texture.h>
+#include <wv/rendering/VertexArrayObject.h>
+#include <wv/rendering/Window.h>
